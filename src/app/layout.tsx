a846096ad587d6a2b38cc969abb44015747e1c1d
arjun <arjunjhukal.makura@gmail.com>
2026-01-19 15:33:54 +0545

    updated the google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
 import { pageSEO } from "@/serverApi/game";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import React from "react";
 import "./globals.css";
 import ProviderWrapper from "./ProviderWrapper";
 import { SeonProvider } from "./SeonProvider";
 import TopLoader from "./TopLoader";
-   
 
 const metadata: Metadata = {
   title: "Sweepstake",
@@ -54,36 +53,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        {GTM_ID && (
-          <Script
-            id="gtm-init"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GTM_ID}');
-              `,
-            }}
-          />
-        )}
-      </head>
       <body className={`${inter.className} scroll-smooth`} cz-shortcut-listen="true">
         {GTM_ID && (
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
+          <GoogleAnalytics gaId={GTM_ID} />
         )}
         <SeonProvider>
           <ProviderWrapper>

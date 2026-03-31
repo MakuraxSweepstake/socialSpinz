c3e78062672c29ad37e609ed5b8dfe5a4c44b902
arjun <arjunjhukal.makura@gmail.com>
2026-03-31 15:04:54 +0545

    fixed the build issue

--- src/app/(dashboard)/(user)/(outsideAuth)/withdrawl/page.tsx
+++ src/app/(dashboard)/(user)/(outsideAuth)/withdrawl/page.tsx
@@ -1,12 +1,9 @@
-"use client"
-
 import WithdrawlPage from '@/components/pages/dashboard/userDashboard/withdrawl';
-import { useGetAllGamesForUserQuery } from '@/services/gameApi';
-import { useGetUserGameBalanceQuery } from '@/services/transaction';
+import { getAllGames, getUserGameBalance } from '@/serverApi/game';
 
 export default async function Withdrawl() {
-    const { data: games } = useGetAllGamesForUserQuery();
-    const { data: coins } = useGetUserGameBalanceQuery();
+    const games = await getAllGames();
+    const coins = await getUserGameBalance();
     return (
         <WithdrawlPage games={games} coins={coins} />
     )
--- src/serverApi/game.ts
+++ src/serverApi/game.ts
@@ -1,5 +1,6 @@
-import { GameResponseProps } from "@/types/game";
+import { CredentialsResponseProps, GameResponseProps } from "@/types/game";
 import { serverBaseQuery } from "./serverBaseQuery";
+import { cookies } from "next/headers";
 
 export async function getAllGames(): Promise<GameResponseProps> {
     return serverBaseQuery("/api/get-games");
@@ -15,4 +16,15 @@ export async function getSupportEmail(): Promise<any> {
 }
 export async function getUsp(): Promise<any> {
     return serverBaseQuery("/api/general/home/usp");
+}
+
+
+export async function getUserGameBalance(): Promise<CredentialsResponseProps> {
+    const cookieStore = await cookies();
+    const access_token = cookieStore.get("access_token")?.value;
+
+    return serverBaseQuery<CredentialsResponseProps>(`/api/detail/get-balance`, {
+        token: access_token,
+        withAuth: true,
+    });
 }
